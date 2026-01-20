7c592000fed9f5bcf7ee2ca8d4bd712d45e48a12
iShunzhi <VIP20200825@163.com>
2026-01-20 16:01:27 +0800

    DTO的基础类

--- src/main/java/cn/shuniverse/base/entity/BaseDto.java
+++ src/main/java/cn/shuniverse/base/entity/BaseDto.java
@@ -1,47 +1,56 @@
 package cn.shuniverse.base.entity;
 
 import cn.shuniverse.base.constants.DateConstants;
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 
 /**
- * Created by 蛮小满Sama at 2025-12-24 15:43
+ * Created by 蛮小满Sama at 2026-01-20 15:57
  *
  * @author 蛮小满Sama
- * @description 基础DTO
+ * @description DTO的基础类
  */
 @Data
-public class BaseDto {
-    /**
-     * 主键
-     */
-    private String id;
+@Schema(description = "基础DTO")
+@Accessors(chain = true)
+public class BaseDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
+    /**
+     * 主键ID
+     */
+    @Schema(description = "主键ID")
+    protected String id;
     /**
      * 创建时间
      */
     @JsonFormat(timezone = DateConstants.GMT8, pattern = DateConstants.DATETIME_FORMAT)
+    @Schema(description = "创建时间")
     protected Date createdAt;
-
     /**
      * 更新时间
      */
     @JsonFormat(timezone = DateConstants.GMT8, pattern = DateConstants.DATETIME_FORMAT)
+    @Schema(description = "更新时间")
     protected Date updatedAt;
 
     /**
      * 创建人
      */
-    @TableField(value = "created_by", fill = FieldFill.INSERT)
+    @Schema(description = "创建人")
     protected String createdBy;
 
     /**
      * 更新人
      */
-    @TableField(value = "updated_by", fill = FieldFill.INSERT_UPDATE)
+    @Schema(description = "更新人")
     protected String updatedBy;
 }
+
